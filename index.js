35699a9222993a33829428b2df1e6c1532b2c9f2
ramdanicareer <ramdanicareer@gmail.com>
2019-07-15 10:04:45 +0700

    create index

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-test
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+const homeRouter = require("./routes/home");
+
+app.use(homeRouter);
+
+app.listen(3000, () => {
+  console.log("server started");
+});
